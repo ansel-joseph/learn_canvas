413f023ed3757727acb0369c311b1143a118e761
Ansel Joseph <anseljoseph2509@gmail.com>
2025-09-10 22:20:47 +0530

    add: circle on every click

--- script.js
+++ script.js
@@ -6,13 +6,23 @@ canvas.height = window.innerHeight;
 window.addEventListener('resize', function(){
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+});
 
-})
-ctx.fillStyle = 'black';
-ctx.strokeStyle = 'red';
-ctx.lineWidth = 3;
-ctx.beginPath();
-ctx.arc(100, 100, 50, 0, Math.PI * 2);
-ctx.fill();
-ctx.stroke();
-console.log(ctx);
+const mouse = {
+    x:null,
+    y:null,
+}
+canvas.addEventListener('click', function(event){
+    mouse.x = event.x;
+    mouse.y = event.y;
+    drawCircle();
+});
+
+function drawCircle(){
+    ctx.fillStyle = 'white';
+    ctx.beginPath();
+    ctx.arc(mouse.x, mouse.y, 20, 0, Math.PI * 2);
+    ctx.fill();
+}
+drawCircle();
+ 
