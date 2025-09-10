062d10883b09b4b64b0971a47f482586377546e0
Ansel Joseph <anseljoseph2509@gmail.com>
2025-09-10 12:11:14 +0530

    add: avoiding stretching of shape

---
+++ script.js
@@ -0,0 +1,13 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.addEventListener('resize', function(){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    ctx.fillStyle = 'white';
+    ctx.fillRect(150,50,100,50)
+})
+ctx.fillStyle = 'white';
+ctx.fillRect(150,50,100,50)
