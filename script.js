fcca0e502caecd7ef3129bddf540199650626e8a
Ansel Joseph <anseljoseph2509@gmail.com>
2025-09-18 23:58:11 +0530

    add: conecting the trails to each other

--- script.js
+++ script.js
@@ -57,18 +57,33 @@ function handleParticles(){
     for(let i=0; i<particlesArray.length; i++){
         particlesArray[i].update();
         particlesArray[i].draw();
+        for(let j=i; j<particlesArray.length; j++){
+            const dx = particlesArray[i].x - particlesArray[j].x;
+            const dy = particlesArray[i].y - particlesArray[j].y;
+            const distance = Math.sqrt(dx*dx + dy*dy);
+            if(distance < 100){
+                ctx.beginPath();
+                ctx.strokeStyle = particlesArray[i].color;
+                ctx.lineWidth = particlesArray[i].size/10;
+                ctx.moveTo(particlesArray[i].x, particlesArray[i].y);
+                ctx.lineTo(particlesArray[j].x, particlesArray[j].y);
+                ctx.stroke();
+                ctx.closePath();
+            }
+        }
         if(particlesArray[i].size <= 0.3){
             particlesArray.splice(i, 1);
             i--;
         }
         
+        
     }
 }
 
 function animate(){
-    //ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = 'rgba(0,0,0,0.02)';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //ctx.fillStyle = 'rgba(0,0,0,0.02)';
+    //ctx.fillRect(0, 0, canvas.width, canvas.height);
     handleParticles();  
     hue+=5;
     requestAnimationFrame(animate);
